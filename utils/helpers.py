7294e50a87c68953a6c1f997c57feba41b9179cb
hafida djelti <h.djelti@laposte.net>
2026-05-05 23:01:32 +0200

    Add rename_columns function to helpers.py

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,4 +1,5 @@
 import requests
+from pyspark.sql import DataFrame
 
 def call_hubeau(endpoint, params):
     """
@@ -9,3 +10,13 @@ def call_hubeau(endpoint, params):
     response = requests.get(url, params=params)
     response.raise_for_status()
     return response.json().get("data", [])
+
+
+def rename_columns(df: DataFrame, mapping: dict) -> DataFrame:
+    """
+    Renomme les colonnes d'un DataFrame Spark selon un mapping.
+    mapping = {"ancienne_colonne": "nouvelle_colonne"}
+    """
+    for old, new in mapping.items():
+        df = df.withColumnRenamed(old, new)
+    return df
